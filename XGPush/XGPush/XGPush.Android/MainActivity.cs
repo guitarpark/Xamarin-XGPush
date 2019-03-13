99a073a21c9cd7994c7c5dac565e442d605117a9
guita <guita@DESKTOP-8O3TOA8>
2019-03-13 22:17:54 +0800

    还是有bug

--- XGPush/XGPush/XGPush.Android/MainActivity.cs
+++ XGPush/XGPush/XGPush.Android/MainActivity.cs
@@ -18,20 +18,19 @@ namespace XGPush.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            Com.Tencent.Android.Tpush.XGPushManager.RegisterPush(this, new CallBack());
-
+            new CallBack().Init();
             base.OnCreate(savedInstanceState);
 
-
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+            Com.Tencent.Android.Tpush.XGPushManager.RegisterPush(this, new CallBack());
         }
 
         public class CallBack : Java.Lang.Object, Com.Tencent.Android.Tpush.IXGIOperateCallback
         {
             public CallBack()
             {
-                var ff = "aaaaaa";
             }
             public void OnFail(Java.Lang.Object p0, int p1, string p2)
             {
@@ -40,7 +39,13 @@ namespace XGPush.Droid
 
             public void OnSuccess(Java.Lang.Object p0, int p1)
             {
-                throw new NotImplementedException();
+                var aaa = p0.ToString();
+                var bb = p1;
+            }
+
+            public void Init()
+            {
+                var dt = DateTime.Now;
             }
         }
     }
